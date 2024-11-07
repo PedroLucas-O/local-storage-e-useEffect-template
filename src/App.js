b23a8c770b41da23ae05650f8a3b6bcee4ab4642
PedroLucas-O <pedrolucas.pc.cp@gmail.com>
2024-11-07 16:55:25 -0300

    Aprendendo a Utilizar da função (localStorage)

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function App() {
+  // Estado para armazenar a lista de compras
+  const [listaCompras, setListaCompras] = useState([]);
+
+  // Estado para armazenar o valor do item sendo digitado
+  const [item, setItem] = useState("");
+
+  // Função para adicionar um item à lista de compras
+  // const adicionarItem = () => {
+  //   if (item.trim() !== "") {
+  //     // Verifica se o item não está vazio ou contém apenas espaços em branco
+  //     setListaCompras([...listaCompras, item]); // Adiciona o item à lista de compras
+  //     setItem(""); // Limpa o campo de entrada
+  //   }
+  // };
+
+  const adicionaTarefa = () => {
+    setListaCompras([...listaCompras,item])
+  }
+
+  const guardarTarefa = () => {
+    const tarefaString = JSON.stringify(listaCompras)
+    localStorage.setItem("item", tarefaString)
+    console.log(tarefaString)
+  }
+
+  const pegarTarefa = () => {
+    const tarefaStorage = localStorage.getItem("item")
+    const tarefaConvertida = JSON.parse(tarefaStorage)
+    setListaCompras(tarefaConvertida)
+  }
+
+
+  return (
+    <div>
+      <h1>Lista de Compras</h1>
+      <input
+        type="text"
+        value={item}
+        onChange={(e) => setItem(e.target.value)}
+        placeholder="Digite um item"
+      />
+      <button onClick={adicionaTarefa}>Adicionar</button>
+      <button onClick={guardarTarefa}>Guardar Tarefa</button>
+      <button onClick={pegarTarefa}>Buscar Tarefa</button>
+
+      <ul>
+        {listaCompras.map((compra, index) => (
+          <li key={index}>{compra}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
